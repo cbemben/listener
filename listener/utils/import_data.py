0f6f30c6de51b4463b6c3149beffe6595c79dfc1
Bemben <christopher.bemben@spectrumhealth.org>
2021-09-13 14:13:31 -0400

    adding functionality to isolate text cols

--- listener/utils/import_data.py
+++ listener/utils/import_data.py
@@ -26,3 +26,17 @@ def import_csv(is_qualtrics_file: bool=False):
     else:
         data = pandas.read_csv(_get_source_file_path())
     return data
+
+def isolate_comment_col(data: pandas.DataFrame, focus_col_list: list):
+    """isolate the raw comment columns with associated row id so we can
+    feed the comments into subsequent pipelines in this package. Use this function
+    if the source file has a lot of additional columns not necessary for text analysis.
+
+    :param pandas.DataFrame data: A dataframe containing unstructured comments
+
+    :param list focus_col_list: the location of the row index and text cols. For example,
+        Qualtrics uses the Response ID for each unique response so we can
+        use this to keep track of the individual comments if we need to join back later.
+        Also, include column indexes for the specific comment cols.
+    """
+    return data.iloc[:, focus_col_list]
