6b111c840b3187225efadff99dbbcc7c97a85c4e
Bemben <christopher.bemben@spectrumhealth.org>
2021-09-16 13:40:27 -0400

    generating output

--- listener/textBlender.py
+++ listener/textBlender.py
@@ -1,12 +1,21 @@
-from spacy.vocab import Vocab
-from spacy.tokenizer import Tokenizer
-from spacy.lang.en import English
+import spacy
+from spacytextblob.spacytextblob import SpacyTextBlob
 
 from listener.utils.import_data import import_csv, isolate_comment_col
 
 data = import_csv(is_qualtrics_file=True)
 comments = isolate_comment_col(data, [9,19,21,23]) 
 
-vocab = Vocab(strings=["hello","world"])
-nlp = English()
-tokenizer = Tokenizer(nlp.vocab)
+nlp = spacy.load('en_core_web_lg')
+nlp.add_pipe('spacytextblob')
+com = comments
+com = com.dropna()
+com["response_text"] = com.iloc[:,1].apply(lambda x: 'What made scheduling the appointment? ' + x)
+com["toks"] = com.iloc[:,1].apply(lambda x: nlp(x))
+com["polarity"] = com["toks"].apply(lambda x: x._.polarity)
+com["subjectivity"] = com["toks"].apply(lambda x: x._.subjectivity)
+com["assessments"] = com["toks"].apply(lambda x: x._.assessments)
+
+from matplotlib import pyplot as plt
+plt.hist(com["polarity"])
+plt.show()
