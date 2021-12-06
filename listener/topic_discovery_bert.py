1399468db67b1da5f915173aa1c704b596039806
Bemben <christopher.bemben@spectrumhealth.org>
2021-12-06 11:00:54 -0500

    adding func to help interpret clusters

--- listener/topic_discovery_bert.py
+++ listener/topic_discovery_bert.py
@@ -1,5 +1,7 @@
 import umap
 import hdbscan
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
 import matplotlib.pyplot as plt
 import pandas as pd
 from sentence_transformers import SentenceTransformer
@@ -56,6 +58,33 @@ class TopicDiscoveryBert:
         plt.scatter(clustered.x, clustered.y, c=clustered.labels, s=0.05, cmap='hsv_r')
         plt.colorbar()
         return plt.show()
+
+    def c_tf_idf(documents, m, ngram_range=(1, 1)):
+        count = CountVectorizer(ngram_range=ngram_range, stop_words="english").fit(documents)
+        t = count.transform(documents).toarray()
+        w = t.sum(axis=1)
+        tf = np.divide(t.T, w)
+        sum_t = t.sum(axis=0)
+        idf = np.log(np.divide(m, sum_t)).reshape(-1, 1)
+        tf_idf = np.multiply(tf, idf)
+        return tf_idf, count
+
+    def extract_top_n_words_per_topic(tf_idf, count, docs_per_topic, n=20):
+        words = count.get_feature_names()
+        labels = list(docs_per_topic.Topic)
+        tf_idf_transposed = tf_idf.T
+        indices = tf_idf_transposed.argsort()[:, -n:]
+        top_n_words = {label: [(words[j], tf_idf_transposed[i][j]) for j in indices[i]][::-1] for i, label in enumerate(labels)}
+        return top_n_words
+
+    def extract_topic_sizes(df):
+        topic_sizes = (df.groupby(['Topic'])
+                         .Doc
+                         .count()
+                         .reset_index()
+                         .rename({"Topic": "Topic", "Doc": "Size"}, axis='columns')
+                         .sort_values("Size", ascending=False))
+        return topic_sizes
 
 if __name__=='__main__':
     import pandas
